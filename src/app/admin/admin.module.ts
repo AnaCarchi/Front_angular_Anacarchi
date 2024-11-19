5dffe5cc7edd11eb8df84a4d92a852c0005a421d
AnaCarchi <agr.carchi@yavirac.edu.ec>
2024-11-19 11:07:47 -0500

    Clase de hoy 19/11/2024/

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AppLayoutModule } from './layout/app.layout.module';
+//import { PerfilComponent } from './components/perfil/perfil.component';
+
+
+@NgModule({
+  declarations: [
+    ClienteComponent,
+    LayoutComponent,
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    AppLayoutModule
+  ]
+})
+export class AdminModule { }
+
